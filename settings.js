e6ba4fcc21aa223f69ec1bbaa53cc4dbd7be65e4
Gopyr <raepits@gmail.com>
2025-09-29 23:38:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ export default {
   image: "https://files.catbox.moe/99wk44.png", // Icon kecil di footer
 
   // Tambahkan ini ↓
-  token: "BOT_TOKEN_DISCORD",
+  token: "MTM5NTI1NzczODE0OTIzNjg5OA.G7oAsV.5Yo2-IoiZLz4ePxp-ePFotDUTYkCPICFXI9zqk",
   appId: "1395257738149236898",
   publicKey: "3b9281a7bebefab562d72840ebd768ce60c0b571908f33386f7a3ed7d760bdb9"
 };
