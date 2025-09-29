f0f017696ff4882575badebc4638051d011fae21
Gopyr <raepits@gmail.com>
2025-09-29 23:47:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 export default {
-  owner: "623514636469272577", // Discord ID owner
+  owner: "11121", // Discord ID owner
   footer: "Gopan.inc",
   image: "https://files.catbox.moe/99wk44.png", // Icon kecil di footer
 
   // Tambahkan ini ↓
-  token: "MTM5NTI1NzczODE0OTIzNjg5OA.G7oAsV.5Yo2-IoiZLz4ePxp-ePFotDUTYkCPICFXI9zqk",
-  appId: "1395257738149236898",
-  publicKey: "3b9281a7bebefab562d72840ebd768ce60c0b571908f33386f7a3ed7d760bdb9"
+  token: "TOKEN_BOT",
+  appId: "APPLICATION_ID",
+  publicKey: "PUBLIC_KEY"
 };
